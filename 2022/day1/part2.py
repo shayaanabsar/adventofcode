e816c75e1e69f757d7c28c8fd90d052174b9d540
shayaanabsar <69465916+shayaanabsar@users.noreply.github.com>
2022-12-01 08:50:09 +0000

    Create part2.py

---
+++ 2022/day1/part2.py
@@ -0,0 +1,14 @@
+with open('in.txt', 'r') as file:
+    data = file.readlines()
+    amounts = []
+    curr = 0
+
+    for line in data:
+        if line == '\n':
+            amounts.append(curr)
+            curr = 0
+        else:
+            line = line.strip('\n')
+            curr += int(line)
+    amounts = sorted(amounts, reverse=True)
+    print(sum(amounts[:3]))

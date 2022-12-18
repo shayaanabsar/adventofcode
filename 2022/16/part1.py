0f9a480493da353c1e273c3835268d18b96463cc
unknown <shayaan.absar@gmail.com>
2022-12-18 00:33:02 +0000

    day 17 part 1

---
+++ 2022/16/part1.py
@@ -0,0 +1,73 @@
+import re
+from collections import deque
+from copy import deepcopy
+
+map = {}
+flow_rates = {}
+
+with open('in.txt', 'r') as f:
+	data = f.readlines()
+	for line in data:
+		line = line.strip('\n').replace(';', ' ').replace(',', '').split()
+		valve, flow_rate, leading_to = line[1], int(line[4][5:]), line[9:]
+		map[valve] = set(leading_to)
+		flow_rates[valve] = flow_rate
+
+valves = list(flow_rates.keys())
+
+distances = {}
+
+for i in range(len(valves)):
+	for j in range(len(valves)):
+		if i == j:
+			distances[(valves[i], valves[j])] = 0
+		elif valves[j] in map[valves[i]]:
+			distances[(valves[i], valves[j])] = 1
+		else:
+			distances[(valves[i], valves[j])] = float('inf')
+
+for k in range(len(valves)):
+	for i in range(len(valves)):
+		for j in range(len(valves)):
+			distances[(valves[i], valves[j])] = min(distances[(valves[i], valves[j])],
+													distances[(valves[i], valves[k])] + distances[(valves[k], valves[j])]
+			)
+
+class State:
+	def __init__(self, curr_valve, open_valves, time, flow):
+		self.curr_valve = curr_valve
+		self.open_valves = open_valves
+		self.time = time
+		self.flow = flow
+	
+	def __repr__(self):
+		return f'(curr_valve: {self.curr_valve}, open_valves: {self.open_valves}, time: {self.time}, flow: {self.flow})'
+
+queue = deque()
+queue.append(State('AA', set(), 0, 0))
+
+for valve in deepcopy(valves):
+	if flow_rates[valve] == 0:
+		valves.remove(valve)
+
+highest = float('-inf')
+
+while queue:
+	curr = queue.popleft()
+
+	if curr.time >= 30:
+		continue
+
+	for valve in valves:
+		if valve != curr.curr_valve and valve not in curr.open_valves:
+			new_time = curr.time + distances[(curr.curr_valve, valve)] + 1
+
+			queue.append(State(
+				valve,
+				curr.open_valves.union({valve}),
+				new_time,
+				curr.flow + ((30 - new_time) * flow_rates[valve])
+			))
+
+	highest = max(highest, curr.flow)
+	print(highest)
---
+++ 2022/17/part1.py
@@ -0,0 +1,76 @@
+with open('in.txt') as f:
+	pattern = f.readline()
+
+rocks = [
+	'####',
+	' # \n###\n # ',
+	'  #\n  #\n###',
+	'#\n#\n#\n#',
+	'##\n##'
+]
+
+grid = set()
+
+STOP = 0
+
+def is_hitting(rock_pos):
+	if rock_pos.intersection(grid) :
+		return True
+	
+	for (i, j) in rock_pos:
+		if i < -3:
+			return True
+		if j < 0 or j >= 7:
+			return True
+	return False
+
+def move_down(rock_pos):
+	new_rock_pos = {(i-1, j) for (i, j) in rock_pos}
+
+	if not is_hitting(new_rock_pos):
+		return new_rock_pos
+	return STOP
+
+def move_sideways(rock_pos, direction):
+	if direction == '>':
+		shift = 1
+	else:
+		shift = -1
+
+	new_rock_pos = {(i, j+shift) for (i, j) in rock_pos}
+
+	if not is_hitting(new_rock_pos):
+		return new_rock_pos
+	return rock_pos
+
+highest = 0
+direction_count = 0
+
+for i in range(2023):
+	rock = rocks[i % 5]
+	rows = rock.split('\n')
+
+	if i >= 1:
+		highest = max(grid, key=lambda x:x[0])[0] + 4
+
+	rock_pos = set()
+	for y, row in enumerate(rows[::-1]):
+		for x, char in enumerate(row):
+			if char == '#':
+				rock_pos.add((highest+y, x+2))
+
+	
+	if i == 0:
+		highest = 0
+	while True:
+		rock_pos = move_sideways(rock_pos, pattern[direction_count % len(pattern)])	
+		direction_count += 1
+		test = move_down(rock_pos)
+
+		if test == STOP:
+			grid = grid.union(rock_pos)
+			break
+		else:
+			rock_pos = test
+		
+print(highest)

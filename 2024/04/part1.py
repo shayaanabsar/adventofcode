c74507441de4ea18c858077c8098239c139d52ad
Shayaan <shayaan.absar@gmail.com>
2024-12-04 15:29:05 +0000

    day 4

--- 2024/04/part1.py
+++ 2024/04/part1.py
@@ -1,1 +1,31 @@
+from copy import copy
+
 with open('in.txt') as f: data = [line.strip() for line in f.readlines()]
+
+positions = []
+
+dirs = [(-1, -1), (-1, 0), (-1, +1),
+		(+0, -1), (+0, +1),
+		(+1, -1), (+1, +0), (+1, +1)]
+
+for i, col in enumerate(data):
+	for j, l in enumerate(col):
+		if l == 'X': positions.append((i, j, None))
+
+for letter in 'MAS':
+	new_positions = []
+	for i, j, dir in positions:
+		if dir is None:
+			for n, d in enumerate(dirs):
+				i_change, j_change = d
+				if (0 <= i+i_change < len(data) ) and (0 <= j+j_change < len(data[0])) and data[i+i_change][j+j_change] == letter:
+					new_positions.append((i+i_change, j+j_change, n))
+				
+		else:
+			i_change, j_change = dirs[dir]
+			if (0 <= i+i_change < len(data)) and (0 <= j+j_change < len(data[0])) and data[i+i_change][j+j_change] == letter:
+				new_positions.append((i+i_change, j+j_change, dir))
+
+	positions = copy(new_positions)
+
+print(len(positions))
--- 2024/04/part2.py
+++ 2024/04/part2.py
@@ -1,1 +1,31 @@
 with open('in.txt') as f: data = [line.strip() for line in f.readlines()]
+
+positions = []
+
+for i, col in enumerate(data):
+	for j, l in enumerate(col):
+		if l == 'A': positions.append((i, j))
+
+diagonals = [
+	[(-1, -1), (0, 0), (+1, +1)],
+	[(-1, +1), (0, 0), (+1, -1)]
+]
+
+count = 0
+
+for i, j in positions:
+	strings = []
+	for diagonal in diagonals:
+		string = ''
+
+		for i_change, j_change in diagonal:
+			new_i, new_j = i+i_change, j+j_change
+
+			if (0 <= new_i < len(data) ) and (0 <= new_j < len(data[0])):
+				string += data[new_i][new_j]
+
+		strings.append(string)
+	
+	if strings[0] in ('SAM', 'MAS') and strings[1] in ('SAM', 'MAS'): count += 1
+
+print(count)
